f6b671bc0b9d7e03d28c4a65a175bf30f98341c4
umadevi-dharmaraj <59370692+umadevi-dharmaraj@users.noreply.github.com>
2023-08-03 21:16:47 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,13 @@
+AFRAME.registerComponent("move-box-on-click",{
+    schema:{
+        moveX: {type:"number",default:1}
+    },
+    tick: function(){
+        window.addEventListener("click",(e)=>{
+            this.data.moveX = this.data.moveX + 0.01;
+        })        
+        var pos = this.el.getAttribute("position");
+        pos.x = this.data.moveX;
+        this.el.setAttribute("position",{x:pos.x,y:pos.y,z:pos.z})
+    }
+})
